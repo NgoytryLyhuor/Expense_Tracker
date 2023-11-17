684bad8142521e6d516a41eb2a40dc3a4cf9d2c7
ngoytrylyhuor <lyhourkhe123@gmail.com>
2023-11-17 17:10:46 +0700

    Edit Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
     <div class="row">
         <div class="col-12">
             <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                <h1 class="mb-sm-0 text-center"> Ngoytry Lyhuor </h1>
+                <h1 class="mb-sm-0 text-center"> Expense Tracker </h1>
             </div>
         </div>
     </div>
